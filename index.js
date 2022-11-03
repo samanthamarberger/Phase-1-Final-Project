95227826ad996a25e108c1e3409daa0d14983080
samanthamarberger <slatham04@gmail.com>
2022-11-03 14:35:05 -0600

    added fetch post

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
     const submitForm = document.getElementsByClassName('add-job-form')[0];
     submitForm.addEventListener('submit', handleSubmit)
 
@@ -10,11 +10,30 @@ document.addEventListener('DOMContentLoaded', () =>{
             position: e.target.position.value,
             imageURL: e.target.image.value
         }
-        console.log(jobObj);
-        e.target.name.value="";
-        e.target.position.value="";
-        e.target.image.value="";
+        //console.log(jobObj);
+        createNewJob(jobObj);
+        e.target.name.value = "";
+        e.target.position.value = "";
+        e.target.image.value = "";
     }
 
-    
+    function createNewJob(jobObject) {
+        fetch('http://localhost:3000/jobs', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            Accept: "application/json"
+            },
+            body: JSON.stringify(jobObject)
+        })
+        .then(res => res.json())
+        .then(job => {
+            addJobToTheDOM(job);
+        })
+    }
+
+    // function addJobToTheDOM(job){
+    //     const div = document.createElement('div');
+    //     div.classList.add('card');
+    // }
 })
