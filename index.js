a308f33f37106b70d61babd717ce31d708e1dbb5
samanthamarberger <slatham04@gmail.com>
2022-11-03 13:52:50 -0600

    added event listener for submit button

---
+++ index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const submitForm = document.getElementsByClassName('add-job-form')[0];
+    submitForm.addEventListener('submit', handleSubmit)
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        //console.log(submitForm);
+        let jobObj = {
+            companyName: e.target.name.value,
+            imageURL: e.target.image.value
+        }
+        console.log(jobObj);
+        e.target.name.value="";
+        e.target.image.value="";
+    }
+    
+})
